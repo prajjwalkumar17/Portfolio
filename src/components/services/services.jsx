2365aff49eb7b102a4010c93752881e81eb1104e
prajjwalkumar17 <talk2prajjwal@gmail.com>
2022-07-01 14:17:57 +0530

    done with services sec

--- src/components/services/services.jsx
+++ src/components/services/services.jsx
@@ -1,9 +1,118 @@
 /** @format */
 import React from "react";
 import "./services.css";
-
+import { AiOutlineCheck } from "react-icons/ai";
 const services = () => {
-  return <section id="services">Services</section>;
+  return (
+    <section id="services">
+      <h5>What I offer</h5>
+      <h2>Sevices</h2>
+      <div className="services__container">
+        <article className="service">
+          <div className="service__head">
+            <h3>UI/UX Design</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <AiOutlineCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+            </li>{" "}
+            <li>
+              <AiOutlineCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+            </li>{" "}
+            <li>
+              <AiOutlineCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+            </li>{" "}
+            <li>
+              <AiOutlineCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+            </li>{" "}
+            <li>
+              <AiOutlineCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+            </li>{" "}
+            <li>
+              <AiOutlineCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+            </li>{" "}
+          </ul>
+        </article>
+        {/*End of UI UX*/}
+
+        <article className="service">
+          <div className="service__head">
+            <h3>Android Dev</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <AiOutlineCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+            </li>{" "}
+            <li>
+              <AiOutlineCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+            </li>{" "}
+            <li>
+              <AiOutlineCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+            </li>{" "}
+            <li>
+              <AiOutlineCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+            </li>{" "}
+            <li>
+              <AiOutlineCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+            </li>{" "}
+            <li>
+              <AiOutlineCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+            </li>{" "}
+            <li>
+              <AiOutlineCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+            </li>{" "}
+          </ul>
+        </article>
+        {/*End of Android*/}
+
+        <article className="service">
+          <div className="service__head">
+            <h3>Backend Dev</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <AiOutlineCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+            </li>{" "}
+            <li>
+              <AiOutlineCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+            </li>{" "}
+            <li>
+              <AiOutlineCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+            </li>{" "}
+            <li>
+              <AiOutlineCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+            </li>{" "}
+            <li>
+              <AiOutlineCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+            </li>{" "}
+            <li>
+              <AiOutlineCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+            </li>{" "}
+          </ul>
+        </article>
+        {/*End of node*/}
+      </div>
+    </section>
+  );
 };
 
 export default services;
