0a6f66886f040be8636b4112849a061c7356ebe5
prajjwalkumar17 <talk2prajjwal@gmail.com>
2023-02-12 21:52:20 +0530

    Changes

--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -8,6 +8,9 @@ import IMG3 from "./../../assets/portfolio3.png";
 import IMG4 from "./../../assets/portfolio4.png";
 import IMG5 from "./../../assets/portfolio5.png";
 import IMG6 from "./../../assets/portfolio6.png";
+import IMG7 from "./../../assets/portfolio7.png";
+import IMG8 from "./../../assets/portfolio8.png";
+import IMG9 from "./../../assets/portfolio9.png";
 
 const data = [
   {
@@ -26,31 +29,52 @@ const data = [
   },
   {
     id: 3,
-    image: IMG3,
-    title: "Cookbook",
-    github: "https://github.com/prajjwalkumar17/CookBook",
-    demo: "https://cookbook-main.netlify.app/",
+    image: IMG7,
+    title: "WallZee",
+    github: "https://github.com/prajjwalkumar17/WallZee",
+    demo: "https://www.loom.com/share/f8a3518bfd714c82bf8776255c3cf9c4",
   },
   {
     id: 4,
+    image: IMG9,
+    title: "Log-Analyzer",
+    github: "https://github.com/prajjwalkumar17/LogAnalyzer",
+    demo: "https://www.loom.com/share/19c5dd868a584d5d8b9e1cb54d97b98d",
+  },
+  {
+    id: 5,
     image: IMG4,
     title: "TEAMOS",
     github: "https://github.com/prajjwalkumar17/Competo",
     demo: "https://play.google.com/store/apps/details?id=com.StartupBBSR.competo",
   },
   {
-    id: 5,
+    id: 6,
     image: IMG5,
     title: "Jobbers API and Android App",
     github: "https://github.com/prajjwalkumar17/Jobbers_Api",
     demo: "https://www.loom.com/share/64b611e565484b799379857570a37b98",
   },
   {
-    id: 6,
+    id: 7,
     image: IMG6,
     title: "Gamethon",
     github: "https://github.com/prajjwalkumar17/gamethon",
     demo: "https://www.loom.com/share/424e3e71b9ae4982a4d0969ac8577715",
+  },
+  {
+    id: 8,
+    image: IMG3,
+    title: "Cookbook",
+    github: "https://github.com/prajjwalkumar17/CookBook",
+    demo: "https://cookbook-main.netlify.app/",
+  },
+  {
+    id: 9,
+    image: IMG8,
+    title: "React-Portfolio",
+    github: "https://github.com/prajjwalkumar17/Portfolio",
+    demo: "https://www.loom.com/share/5e458f6ffad04c17b830f0ba353dcb91",
   },
 ];
 
